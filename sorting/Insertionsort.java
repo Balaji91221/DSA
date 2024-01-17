796305dd3b86be9311786b19fdb7eeac674c64d3
K Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2024-01-17 12:48:09 +0530

    Insertionsort done

---
+++ sorting/Insertionsort.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Insertionsort {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Insertionsort sorter = new Insertionsort();
+        
+        // Call the sort method
+        sorter.sort(arr);
+
+        // Print the sorted array
+        sorter.printArray(arr, n);
+    }
+
+    public void sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    void printArray(int[] arr, int size) {
+        System.out.print("Sorted array: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
