3248a3cc1883ef74b83ff9625b7f5e8b4782e05f
K Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2024-03-28 23:13:28 +0530

    +/greedy

---
+++ greedy/mincoin.java
@@ -0,0 +1,21 @@
+package greedy;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class mincoin {
+    public static void main(String[] args) {
+        Integer[] coins = {5,10,2,1};
+        int amount = 57;
+        Arrays.sort(coins, Collections.reverseOrder());
+        int res = 0;
+        for (Integer coin : coins) {
+            if (coin <= amount) {
+                int c = (int) Math.floor(amount / coin);
+                res += c;
+                amount -= c * coin;
+            }
+            if (amount == 0) break;
+        }
+        System.out.println(res);
+    }
+}
